230f5c4bed1df704ea4d93d59843a358807f62a6
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2026-01-07 14:12:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vaja teha string array H, e, l, l, o
+            //kasutada for loopi
+
+
+            string[] array = { "H", "e", "l", "l", "o" };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            //tuleb kasutada foreachi
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
